b0ad1405cd7cd384cf426ab5d24e9da53080c38e
Aldi Gunawan <aldi.gunawan@liquidpay.com>
2017-06-09 10:32:22 +0700

    eafwe

--- test-git/app/src/main/java/com/test/test_git/MainActivity.java
+++ test-git/app/src/main/java/com/test/test_git/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String testst = "Yoi mamen";
+        String test = "test";
+        String tests = "gateli cok";
     }
 }
