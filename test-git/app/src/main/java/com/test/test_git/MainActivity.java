150f66afe36e6aca2fbf66a7e149561f5c1cbefa
Aldi Gunawan <aldi.gunawan@liquidpay.com>
2017-06-09 10:34:24 +0700

    remove unused

--- test-git/app/src/main/java/com/test/test_git/MainActivity.java
+++ test-git/app/src/main/java/com/test/test_git/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String testst = "Yoi mamen";
         String test = "test";
         String tests = "gateli cok";
     }
